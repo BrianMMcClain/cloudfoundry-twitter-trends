2e778b29d759ddb896b795f5bf9cce651c1191b7
Brian McClain <brianmmcclain@gmail.com>
2012-05-13 12:14:51 -0400

    Added twitter-stream-node module

--- farmer/farmer.js
+++ farmer/farmer.js
@@ -1,4 +1,7 @@
-var stream = require('twitter-stream');
+var stream = require('./lib/twitter-stream-node/lib/stream');
+
+var config = require('./config')
+
 stream.public(username, password, function(tweet) {
     console.log(tweet.text);
 });
