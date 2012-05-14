7fe487e057e48e62af4f1aa1aa040c1dbf3a2e94
Brian McClain <brianmmcclain@gmail.com>
2012-05-13 23:13:42 -0400

    Farmer now collects tweets and publishes them to a message queue

--- farmer/farmer.js
+++ farmer/farmer.js
@@ -1,7 +1,43 @@
+var amqp = require('amqp');
+var htmlEscape = require('sanitizer/sanitizer').escape;
+
 var stream = require('./lib/twitter-stream-node/lib/stream');
 
 var config = require('./config');
 
-stream.public(config["username"], config["password"], function(tweet) {
-    console.log(tweet.text);
-});
+function rabbitUrl() {
+  if (process.env.VCAP_SERVICES) {
+    conf = JSON.parse(process.env.VCAP_SERVICES);
+    return conf['rabbitmq-2.4'][0].credentials.url;
+  }
+  else {
+    return "amqp://localhost";
+  }
+}
+
+function setup() {
+
+  var exchange = conn.exchange('twitter-farmer', {'type': 'fanout', durable: false}, function() {
+
+    var queue = conn.queue('', {durable: false, exclusive: true},
+    function() {
+      queue.subscribe(function(msg) {
+        console.log(msg);
+      });
+      queue.bind(exchange.name, '');
+    });
+    queue.on('queueBindOk', function() { gatherTweets(exchange); });
+  });
+}
+
+function gatherTweets(exchange) {
+	stream.public(config["username"], config["password"], function(tweet) {
+	    console.log(tweet.text);
+		exchange.publish('', {body: tweet.text});
+	});
+}
+
+var conn = amqp.createConnection({url: rabbitUrl()});
+conn.on('ready', setup);
+
+
